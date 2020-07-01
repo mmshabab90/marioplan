078837330972b1abec3397c8fa866e902cf19244
Meeyad Mohd. Shabab <mmshabab90@gmail.com>
2020-07-01 09:15:17 +0200

    added all blank components

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,14 @@ import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
 import './App.css'
 import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                This is the app component
-            </div>
-        )
+          <BrowserRouter>
+            <div>This is the app component</div>
+          </BrowserRouter>
+        );
     }
 }
