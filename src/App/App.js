393ac5ab6a5eb2a4031bb9da2bfa448046182637
Meeyad Mohd. Shabab <mmshabab90@gmail.com>
2020-07-01 09:04:11 +0200

    initial react project with necessary packages

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
+import './App.css'
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                This is the app component
+            </div>
+        )
+    }
+}
